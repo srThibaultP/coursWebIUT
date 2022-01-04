063f64a6d4fc63b662526d99446ffdd74649a5d8
Thibault Pech <thibaultpech@outlook.com>
2022-01-04 10:16:06 +0100

    Ajout bootstrap

--- exo2/index.php
+++ exo2/index.php
@@ -1,18 +1,23 @@
 <html>
-<form action="index.php" method="POST">
-    </li>
 
-    <p>
-        <label>Identifiant :</label>
-        <input type="text" name="user" />
-    </p>
-    <p>
-        <label>Mot de passe :</label>
-        <input type="password" name="pass" />
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
 
-        <input type="submit" value="Connexion" />
-    </p>
-</form>
+    <form action="index.php" method="POST">
+        </li>
+
+        <p>
+            <label>Identifiant :</label>
+            <input type="text" name="user" />
+        </p>
+        <p>
+            <label>Mot de passe :</label>
+            <input type="password" name="pass" />
+
+            <input type="submit" value="Connexion" />
+        </p>
+    </form>
 
 </html>
 
@@ -22,14 +27,12 @@ $pass = $_POST["pass"];
 
 try {
     $dbh = new PDO('mysql:host=localhost;dbname=sakila', $user, $pass);
-    echo "<p>Connexion réussie</p><table border=1>";
+    echo "<p>Connexion réussie</p><table border=1 class='table table-danger table-striped'>";
     foreach ($dbh->query('SELECT * from actor LIMIT 20') as $row) {
         //print_r($row);
         echo "<tr>
-            <td>" . $row["actor_id"] . "</td>
             <td>" . $row["first_name"] . "</td>
             <td>" . $row["last_name"] . "</td>
-            <td>" . $row["last_update"] . "</td>
             </tr>";
     }
     $dbh = null;
